dc32559930330b664dba254ea52357a119eed426
Carlos Sanchez <carlos.sanchez@uepa.com>
2024-06-24 23:34:07 -0400

    Testing

--- SISDEN/WeatherForecast.cs
+++ SISDEN/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace SISDEN
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int Year { get; set; }
     }
 }

c666b75a498da33a523973477de383278a2c1b77
SadaybyM <sh17-1018@unphu.edu.do>
2024-07-22 09:39:27 -0400

    sada prueba

---
+++ SISDEN/Models/VistaUbicacione.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SISDEN.Models;
+
+public partial class VistaUbicacione
+{
+    public int Idubicacion { get; set; }
+
+    public string? Ubdescripcion { get; set; }
+
+    public string? EntidadSector { get; set; }
+}

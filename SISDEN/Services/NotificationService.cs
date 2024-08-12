f6bc37c8c0a541655102e14d6bd2ec77ed9d6390
Victor Jose Rosa Depratt <vr19-1028@unphu.edu.do>
2024-08-11 22:51:29 -0400

    hola

--- SISDEN/Services/NotificationService.cs
+++ SISDEN/Services/NotificationService.cs
@@ -19,30 +19,30 @@ namespace SISDEN.Services
             _hubContext = hubContext;
         }
 
-        //public async Task CreateNotificationAsync(int userId, int estadoId, string message)
-        //{
-        //    var notification = new Notificacion
-        //    {
-        //        Idusuario = userId,
-        //        Idestado = estadoId,
-        //        Mensaje = message,
-        //        Fechaenvio = DateTime.Now,
-        //        Leido = false
-        //    };
+        public async Task CreateNotificationAsync(int userId, int estadoId, string message)
+        {
+            var notification = new Notificacion
+            {
+                Idusuario = userId,
+                Idestado = estadoId,
+                Mensaje = message,
+                Fechaenvio = DateTime.Now,
+                Leido = false
+            };
 
-        //    _context.Notificacions.Add(notification);
-        //    await _context.SaveChangesAsync();
+            //_context.Notificacions.Add(notification);
+            await _context.SaveChangesAsync();
 
-        //    var user = await _context.Usuarios.FindAsync(userId);
-        //    if (user != null)
-        //    {
-        //        // Enviar correo electrónico
-        //        await _emailService.SendEmailAsync(user.Usuemail, "Actualización de Estado de Denuncia", message);
+            var user = await _context.Usuarios.FindAsync(userId);
+            if (user != null)
+            {
+                // Enviar correo electrónico
+                await _emailService.SendEmailAsync(user.Usuemail, "Actualización de Estado de Denuncia", message);
 
-        //        // Enviar notificación en tiempo real
-        //        await _hubContext.Clients.User(userId.ToString()).SendAsync("ReceiveNotification", message);
-        //    }
-        //}
+                // Enviar notificación en tiempo real
+                await _hubContext.Clients.User(userId.ToString()).SendAsync("ReceiveNotification", message);
+            }
+        }
     }
 
 }
